87f8a664c68a9f963a8dfb08ea9bdd1f60e14016
Dmitriy Kolodchenko <dima.kolod@gmail.com>
2022-04-22 21:31:27 +0300

    task2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,32 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// const createMarkup = text => {
+//   const itemEl = document.createElement('li');
+//   itemEl.textContent = text;
+//   itemEl.classList.add('item');
+//   return itemEl;
+// };
+
+// const ingredientsListEl = ingredients.map(ingredient =>
+//   createMarkup(ingredient)
+// );
+
+// const placeToAppend = document.querySelector(
+//   '#ingredients'
+// );
+
+// placeToAppend.append(...ingredientsListEl);
+
+
+const ingredientsListEl = ingredients.map(ingredient => {
+    const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+  itemEl.classList.add('item');
+  return itemEl;
+  });
+
+const placeToAppend = document.querySelector('#ingredients');
+
+placeToAppend.append(...ingredientsListEl)
