d93284108f0504d31521e212b35f69ef2f8cacd4
Dmitriy Kolodchenko <dima.kolod@gmail.com>
2022-04-24 23:55:49 +0300

    task8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const userEmail = event.currentTarget.elements.email.value;
+    const userPass = event.currentTarget.elements.password.value;
+    const userData = {
+        userEmail,
+        userPass,
+    }
+
+    console.log(userData);
+
+    form.reset()
+
+    if (userEmail === '' || userPass === '') {
+        alert('Всі поля повинні бути заповнені')
+    }
+  
+    
+}
